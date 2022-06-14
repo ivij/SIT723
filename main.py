573ea3ed11d91214f382008b626dc748f9618f76
ivij <53103290+ivij@users.noreply.github.com>
2022-06-14 20:14:51 +1000

    Add files via upload

--- main.py
+++ main.py
@@ -1,35 +1,51 @@
 import gym
-from agent_dqn import Agent
-from utils import plotLearning
 import numpy as np
-
+from agent_ppo import Agent
+from utils import plot_learning_curve
+import time
 if __name__ == '__main__':
-    env = gym.make('LunarLander-v2')
-    agent = Agent(gamma=0.99, epsilon=1.0, batch_size=64, n_actions=4, eps_end=0.01,
-                  input_dims=[8], lr=0.001)
-    scores, eps_history = [], []
+    env = gym.make('CartPole-v1')
+    N = 20
+    batch_size = 5
+    n_epochs = 4
+    alpha = 0.0003
+    agent = Agent(n_actions=env.action_space.n, batch_size=batch_size,
+                    alpha=alpha, n_epochs=n_epochs,
+                    input_dims=env.observation_space.shape)
     n_games = 1000
 
+    figure_file = 'plots/cartpole.png'
+
+    best_score = env.reward_range[0]
+    score_history = []
+
+    learn_iters = 0
+    avg_score = 0
+    n_steps = 0
+    start = time.time()
     for i in range(n_games):
-        score = 0
-        done = False
         observation = env.reset()
+        done = False
+        score = 0
         while not done:
-            action = agent.choose_action(observation)
+            action, prob, val = agent.choose_action(observation)
             observation_, reward, done, info = env.step(action)
+            n_steps += 1
             score += reward
-            agent.store_transition(observation, action, reward,
-                                   observation_, done)
-            agent.learn()
+            agent.remember(observation, action, prob, val, reward, done)
+            if n_steps % N == 0:
+                agent.learn()
+                learn_iters += 1
             observation = observation_
-        scores.append(score)
-        eps_history.append(agent.epsilon)
+        score_history.append(score)
+        avg_score = np.mean(score_history[-100:])
 
-        avg_score = np.mean(scores[-100:])
+        if avg_score > best_score:
+            best_score = avg_score
+            agent.save_models()
 
-        print('episode ', i, 'score %.2f' % score,
-              'average score %.2f' % avg_score,
-              'epsilon %.2f' % agent.epsilon)
-    x = [i + 1 for i in range(n_games)]
-    filename = 'lunar_lander.png'
-    plotLearning(x, scores, eps_history, filename)
+        print('episode', i, ', score %.1f' % score, ', avg score %.1f' % avg_score)
+    x = [i+1 for i in range(len(score_history))]
+    plot_learning_curve(x, score_history, figure_file)
+    end = time.time()
+    print(end - start)
